8f63fd607700f5970f99cd46ef121554e8e52c9a
Luciana Dughetti <ludughetti@gmail.com>
2024-06-22 22:26:26 -0300

    Added PlayerPref for high score tracking

--- Clicker/Assets/Scripts/GameController.cs
+++ Clicker/Assets/Scripts/GameController.cs
@@ -11,13 +11,17 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject creditsScreen;
     [SerializeField] private float maxTime = 10f;
 
+    private static string _prefKey_highScore = "HighScore";
+
     private bool _isGameStarted;
     private float _currentTimer;
-    private float _currentClicks;
+    private int _currentClicks;
+    private int _highestScoreSaved;
 
     private void Awake()
     {
         SetupData();
+        FetchHighScore();
     }
 
     private void Update()
@@ -26,10 +30,12 @@ public class GameController : MonoBehaviour
             && _currentTimer > 0)
         {
             _currentTimer -= Time.deltaTime;
-            timeCounter.text = GetFormattedTime();
+            timeCounter.text = GetFormattedTimeToDisplay();
+            highScoreCounter.text = GetHighestScoreToDisplay();
 
             if(_currentTimer <= 0)
             {
+                UpdateHighScore();
                 StartCoroutine(Cooldown());
                 startGameInstruction.gameObject.SetActive(true);
             }
@@ -71,7 +77,7 @@ public class GameController : MonoBehaviour
         _currentClicks = 0;
     }
 
-    private string GetFormattedTime()
+    private string GetFormattedTimeToDisplay()
     {
         int seconds = (int) _currentTimer;
         int milliseconds = (int)((_currentTimer - seconds) * 100);
@@ -79,9 +85,33 @@ public class GameController : MonoBehaviour
         return string.Format("{0:00}:{1:00}", seconds, milliseconds);
     }
 
+    private string GetHighestScoreToDisplay()
+    {
+        return _currentClicks > _highestScoreSaved 
+            ? _currentClicks.ToString() 
+            : _highestScoreSaved.ToString();
+    }
+
     private IEnumerator Cooldown()
     {
         yield return new WaitForSeconds(2);
         _isGameStarted = false;
+    }
+
+    private void FetchHighScore()
+    {
+        _highestScoreSaved = PlayerPrefs.HasKey(_prefKey_highScore) 
+            ?  PlayerPrefs.GetInt(_prefKey_highScore) 
+            : 0;
+    }
+
+    private void UpdateHighScore()
+    {
+        if(_currentClicks > _highestScoreSaved)
+        {
+            _highestScoreSaved = _currentClicks;
+            PlayerPrefs.SetInt(_prefKey_highScore, _currentClicks);
+            PlayerPrefs.Save();
+        }
     }
 }
